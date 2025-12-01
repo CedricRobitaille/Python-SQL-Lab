44cdcf54b6e4616690e028bcf0cb863768d8a787
Cedric <crubbertire@rogers.com>
2025-12-01 13:00:26 -0500

    Root panel input selection

--- crm.py
+++ crm.py
@@ -4,14 +4,44 @@
 
 import sqlite3
 connection = sqlite3.connect("crm_db.db")
-
+print("Connected!")
 cursor = connection.cursor()
-cursor.execute(
-  "CREATE TABLE companies (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(16))"
-)
-cursor.execute(
-  "CREATE TABLE employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32), company_id, INT)"
-)
+## Create Tables into the SQLite CRM-DB
+# cursor.execute(
+#   "CREATE TABLE companies (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(16))"
+# )
+# cursor.execute(
+#   "CREATE TABLE employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(32), company_id, INT)"
+# )
 
+
+print("Welcome to your Customer Relationship Management Tool.")
+
+def companiesPortal():
+  print("\n\n\nCompanies Portal")
+
+def employeesPortal():
+  print("\n\n\nEmployees Portal")
+
+def root():
+  print("Select the portal you would like to enter:")
+  print("1. Companies\n2. Employees")
+  selection = input("> ")
+
+  if selection == "1" or selection == "Companies" or selection == "companies":
+    companiesPortal()
+
+  elif selection == "2" or selection == "Employees" or selection == "employees":
+    employeesPortal()
+
+  else:
+    print("Invalid Selection. Please try again.")
+    root()
+
+
+root()
+
+print("Finished All Actions")
 
 connection.close()
+print("Connection Closed")
